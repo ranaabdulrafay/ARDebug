76f11d0a4b38b6e9e62839820e1cead798d61dee
ranaabdulrafay <ranaabdulrafay@hotmail.com>
2023-02-17 17:18:19 +0500

    added project
    custom logger with color and disable by will to disable logs on build (option) or anytime we like

---
+++ CustomDebugger/Assets/AR/Utility/Debug/Scripts/ARDebug.cs
@@ -0,0 +1,100 @@
+﻿using UnityEditor;
+using UnityEngine;
+namespace AR.Utility
+{
+    public static class ARDebug
+    {
+        public static bool DebugEnabled = true;
+
+        public static void Log(string msg, bool AlwaysLog = false)
+        {
+            if (DebugEnabled || AlwaysLog)
+                Debug.Log(msg);
+        }
+        public static void Log(object msg, bool AlwaysLog = false)
+        {
+            if (DebugEnabled || AlwaysLog)
+                Debug.Log(msg);
+        }
+        public static void Log(object msg,Object context, bool AlwaysLog = false)
+        {
+            if (DebugEnabled || AlwaysLog)
+                Debug.Log(msg, context);
+        }
+
+
+        public static void Log(string msg, Color color, bool AlwaysLog = false)
+        {
+            if (DebugEnabled || AlwaysLog)
+                Debug.Log("<color=#" + ColorUtility.ToHtmlStringRGB(color) + ">" + msg + "</color>");
+        }
+        public static void Log(object msg, Color color, bool AlwaysLog = false)
+        {
+            if (DebugEnabled || AlwaysLog)
+                Debug.Log("<color=#" + ColorUtility.ToHtmlStringRGB(color) + ">" + msg + "</color>");
+        }
+        public static void Log(object msg, Object context, Color color, bool AlwaysLog = false)
+        {
+            if (DebugEnabled || AlwaysLog)
+                Debug.Log("<color=#" + ColorUtility.ToHtmlStringRGB(color) + ">" + msg + "</color>", context);
+        }
+
+
+        public static void LogWarning(string msg, bool AlwaysLog = false)
+        {
+            if (DebugEnabled || AlwaysLog)
+                Debug.LogWarning(msg);
+        }
+        public static void LogWarning(object msg, bool AlwaysLog = false)
+        {
+            if (DebugEnabled || AlwaysLog)
+                Debug.LogWarning(msg);
+        }
+        public static void LogWarning(object msg, Object context, bool AlwaysLog = false)
+        {
+            if (DebugEnabled || AlwaysLog)
+                Debug.LogWarning(msg, context);
+        }
+
+
+        public static void LogError(string msg, bool AlwaysLog = false)
+        {
+            if (DebugEnabled || AlwaysLog)
+                Debug.LogError(msg);
+        }
+        public static void LogError(object msg, bool AlwaysLog = false)
+        {
+            if (DebugEnabled || AlwaysLog)
+                Debug.LogError(msg);
+        }
+        public static void LogError(string msg, Object context, bool AlwaysLog = false)
+        {
+            if (DebugEnabled || AlwaysLog)
+                Debug.LogError(msg, context);
+        }
+
+#if UNITY_EDITOR
+        [MenuItem("ARLog/EnableLog")]
+        static void EnableLog()
+        {
+            DebugEnabled = true;
+        }
+        [MenuItem("ARLog/DisableLog")]
+        static void DisableLog()
+        {
+            DebugEnabled = false;
+        }
+
+        [MenuItem("ARLog/EnableLog", true)]
+        static bool ValidateEnableLog()
+        {
+            return !DebugEnabled;
+        }
+        [MenuItem("ARLog/DisableLog", true)]
+        static bool ValidateDisableLog()
+        {
+            return DebugEnabled;
+        }
+#endif
+    }
+}
---
+++ CustomDebugger/Assets/AR/Utility/Debug/Scripts/ARLogDemo.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using TMPro;
+using AR.Utility;
+using System;
+using UnityEngine.UI;
+
+public class ARLogDemo : MonoBehaviour
+{
+    public enum PresetColors { None, Red, Blue, Green, Orange }
+    public PresetColors m_Color;
+    public bool LogOverride { get; set; }
+    public TMP_InputField m_Input;
+
+    public int m_InputInt;
+    public float m_InputFloat;
+    public bool m_InputBool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void LogValues()
+    {
+        object object1 = string.Format("int: {0} ", m_InputInt);
+        object object2 = string.Format("float: {0} ", m_InputFloat);
+        object object3 = string.Format("bool: {0} ", m_InputBool);
+        switch (m_Color)
+        {
+            case PresetColors.Red:
+                ARDebug.Log(string.Format("{0} , {1} and {2} ", object1, object2, object3), Color.red, LogOverride);
+                break;
+            case PresetColors.Green:
+                ARDebug.Log(string.Format("{0} , {1} and {2} ", object1, object2, object3), Color.green, LogOverride);
+                break;
+            case PresetColors.Blue:
+                ARDebug.Log(string.Format(" {0} , {1} and {2} ", object1, object2, object3), Color.blue, LogOverride);
+                break;
+            case PresetColors.None:
+                ARDebug.Log(string.Format(" {0} , {1} and {2} ", object1, object2, object3), LogOverride);
+                break;
+        }
+    }
+    public void LogInput()
+    {
+        switch (m_Color)
+        {
+            case PresetColors.Red:
+                ARDebug.Log(m_Input.text, Color.red, LogOverride);
+                break;
+            case PresetColors.Green:
+                ARDebug.Log(m_Input.text, Color.green, LogOverride);
+                break;
+            case PresetColors.Blue:
+                ARDebug.Log(m_Input.text, Color.blue, LogOverride);
+                break;
+            case PresetColors.None:
+                ARDebug.Log(m_Input.text, LogOverride);
+                break;
+        }
+    }
+
+    public void SelectOption(Int32 _value)
+    {
+        m_Color = (PresetColors)_value;
+    }
+
+}
